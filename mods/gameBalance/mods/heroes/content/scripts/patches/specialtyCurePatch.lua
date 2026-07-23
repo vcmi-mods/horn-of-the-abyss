b096396f8f099aa649318c4997e7bd080dd50a73
Julian Barth <karyoplasma@proton.me>
2026-07-23 03:09:56 +0200

    update cure patch

--- mods/gameBalance/mods/heroes/content/scripts/patches/specialtyCurePatch.lua
+++ mods/gameBalance/mods/heroes/content/scripts/patches/specialtyCurePatch.lua
@@ -1,15 +1,14 @@
 local Script = setmetatable({}, {__index = Base})
 Script.__index = Script
 
-local function applyHeroSpecialty(mechanics, unit)
-	local base = mechanics:getEffectValue()
-	local hero = mechanics:getHeroCaster()
-
-	if not hero then return base end
+local function getAdjustedEffectValue(mechanics, unit)
+	local base = mechanics:applySpellBonus(mechanics:getEffectValue(), unit)
 	if mechanics:getSpell():getJsonKey() ~= "core:cure" then return base end
 
-	local tier = math.min(math.max(unit:creatureLevel(), 1), 7)
+	local hero = mechanics:getHeroCaster()
+	if not hero then return base end
 
+	local tier = math.min(math.max(unit:creatureLevel(), 1), 7)
 	local specialty = hero:getBonuses(function(b)
 		return b:getType() == "SPECIALTY_CURE"
 	end)
@@ -27,7 +26,7 @@ function Script:getHealthChange(mechanics, spellTarget)
 		local unit = dest.unit
 		if unit then
 			local copy = unit:copy()
-			local effectValue = applyHeroSpecialty(mechanics, unit)
+			local effectValue = getAdjustedEffectValue(mechanics, unit)
 			local healedHP, resurrected = copy:heal(effectValue, self:getHealLevel(), self:getHealPower())
 			result.hpDelta   = result.hpDelta   + healedHP
 			result.unitsDelta = result.unitsDelta + resurrected
@@ -44,9 +43,8 @@ function Script:apply(mechanics, server, target)
 	for _, dest in ipairs(target) do
 		local unit = dest.unit
 		if unit then
-			local effectValue = applyHeroSpecialty(mechanics, unit)
-			local healedHP, resurrected = server:healUnit(
-				battle, unit, effectValue, self:getHealLevel(), self:getHealPower())
+			local effectValue = getAdjustedEffectValue(mechanics, unit)
+			local healedHP, resurrected = server:healUnit(battle, unit, effectValue, self:getHealLevel(), self:getHealPower())
 
 			if resurrected > 0 then
 				local textID = resurrected == 1 and "core.genrltxt.117" or "core.genrltxt.116"
