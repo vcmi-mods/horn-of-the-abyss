7babd8d94cd71535020638acff53120c9e20c874
Julian Barth <karyoplasma@proton.me>
2026-08-18 13:14:08 +0200

    fix animation onUnitSpellcast

--- mods/bulwark/content/scripts/combat/runes.lua
+++ mods/bulwark/content/scripts/combat/runes.lua
@@ -170,7 +170,7 @@ function Script:addYetiRuneLevels(server, battle, unit, oldLevel, amount)
 	return self:addRuneLevel(server, battle, unit, oldLevel, amount, RUNE_TYPES.yeti, 9)
 end
 
-function Script:processRuneGain(server, battle, unit, amount, animate)
+function Script:processRuneGain(server, battle, unit, amount, deferAnimation)
 	if not unit:isAlive() then return end
 	local isYeti = self.isYeti
 	local heroRuneLevel, yetiRuneLevel = self:getCurrentRuneLevels(unit)
@@ -188,8 +188,8 @@ function Script:processRuneGain(server, battle, unit, amount, animate)
 	end
 
 	local animation = ANIMATIONS[newLevel]
-	if animation and animate then
-		server:showBattleAnimation(	battle,	{ { unit = unit } }, animation,	SOUND, 1.0)
+	if animation then
+		server:showBattleAnimation(battle, { { unit = unit } }, animation, SOUND, 1.0, deferAnimation)
 	end
 
 	self:describe(server, battle, unit, newLevel)
@@ -242,23 +242,23 @@ end
 function Script:onAfterAttack(server, battle, unit, other, payload)
 	if payload.isCounter then return end
 	if payload.attackIndex ~= 0 then return end
-	self:processRuneGain(server, battle, unit, 1, true)
+	self:processRuneGain(server, battle, unit, 1, false)
 end
 
 --- Called after `unit` was attacked by `other`.
 function Script:onAfterAttacked(server, battle, unit, other, payload)
 	if payload.attackIndex ~= 0 then return end
-	self:processRuneGain(server, battle, unit, 2, true)
+	self:processRuneGain(server, battle, unit, 2, false)
 end
 
 --- Called when `unit` defends.
 function Script:onDefend(server, battle, unit, other)
-	self:processRuneGain(server, battle, unit, 3, true)
+	self:processRuneGain(server, battle, unit, 3, false)
 end
 
 --- Called when `unit` casts a spell.
 function Script:onUnitSpellcast(server, battle, unit, other)
-	self:processRuneGain(server, battle, unit, 1, false)
+	self:processRuneGain(server, battle, unit, 1, true)
 end
 
 --- Called once for every unit present when the battle starts, after tactics are over.
